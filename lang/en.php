b8e08ddcd222171d038cf82eb89deed294712a19
Luke <17616949+nice42q@users.noreply.github.com>
2023-05-15 09:05:56 +0200

    added languages

---
+++ lang/en.php
@@ -0,0 +1,67 @@
+<?php
+// Für die HTML meta angabe, bspw. <!DOCTYPE html><html lang="de">
+$lang_meta = "en";
+
+$page_title = "XMR conversion to EUR/BTC/CHF/USD and many more"; // Der Browser Tab Titel bzw. Suchmaschinen Titel
+$meta_description = "The Monero live exchange rate in many different currencies, free for everyone."; // Suchmaschinen Beschreibung / Text
+$meta_keywords = "Monero, XMR, fiat, value, rate, live, exchange, conversion"; // Suchmaschinen Keywords (angeblich nutzlos)
+
+$title_h1 = "conversion to"; // ↓ XMR umrechnung nach ↓
+
+// Info Text
+$info = "The exchange rates on this site are for information purposes only. They are not guaranteed to be accurate, and are subject to change without notice. The exchange rates are updated every 5 Seconds. Last at <u>".$time."</u> clock, Europe/Berlin. Data provided by <a class='text-white' href='https://www.coingecko.com/en/coins/monero' hreflang='en' rel='external' target='_blank'>CoinGecko</a>.<br/><a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>Github</a>";
+
+// Tooltip Titel
+$l_eur = "Euro";
+$l_btc = "Bitcoin";
+$l_chf = "Swiss Franc";
+$l_usd = "US Dollar";
+$l_ltc = "Litecoin";
+$l_gbp = "Pound sterling";
+$l_rub = "Russian Ruble";
+$l_jpy = "Yen";
+$l_try = "Turkish Lira";
+$l_cad = "Canadian Dollar";
+$l_aud = "Australian Dollar";
+$l_hkd = "Hong Kong Dollar";
+$l_sgd = "Singapore Dollar";
+$l_pln = "Złoty";
+$l_zar = "South African Rand";
+$l_inr = "Indian Rupee";
+$l_aed = "UAE Dirham";
+$l_eth = "Ethereum";
+$l_uah = "Hryvnia"; 
+$l_krw = "South Korean won";
+$l_brl = "Brazilian Real"; 
+$l_myr = "Malaysian Ringgit"; 
+$l_cny = "Renminbi"; 
+$l_xag = "Silver (troy ounce)";
+$l_xau = "Gold (troy ounce)";
+$l_vnd = "Vietnamese dong";
+$l_vef = "Venezuelan bolívar";
+$l_thb = "Baht";
+$l_sar = "Saudi Riyal";
+$l_sek = "Shwedish Krona";
+$l_pkr = "Pakistani Rupee";
+$l_nzd = "New Zealand Dollar";
+$l_php = "Philippine peso";
+$l_nok = "Norwegian Krone";
+$l_lkr = "Sri Lankan Rupee";
+$l_mmk = "Myanmar Kyat";
+$l_huf = "Hungarian Forint";
+$l_ils = "Israeli New Shekel";
+$l_kwd = "Kuwaiti Dinar";
+$l_ngn = "Nigerian Naira";
+$l_idr = "Indonesian Rupiah";
+$l_twd = "New Taiwan Dollar";
+$l_ars = "Argentine Peso";
+$l_bdt = "Bangladeshi Taka";
+$l_bhd = "Bahraini Dinar"; 
+$l_bmd = "Bermudan Dollar"; 
+$l_czk = "Czech Koruna";
+$l_dkk = "Danish Krone";
+
+// Weitere Monero Links
+$getmonero = '<a class="text-white" href="https://www.getmonero.org/" hreflang="en" target="_blank" rel="external">Official website</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Community Crowdfunding System (CCS)</a> | <a class="text-white" href="https://moneroecosystem.org/" hreflang="en" target="_blank" rel="external">Monero Ecosystem</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a> | <a class="text-white" href="https://t.me/monero" hreflang="en" target="_blank" rel="external">Telegram - Monero XMR</a>';
+
+?>
---
+++ lang/it.php
@@ -0,0 +1,67 @@
+<?php
+// Für die HTML meta angabe, bspw. <!DOCTYPE html><html lang="de">
+$lang_meta = "it";
+
+$page_title = "Conversione XMR in EUR/BTC/CHF/USD e molti altri"; // Der Browser Tab Titel bzw. Suchmaschinen Titel
+$meta_description = "Il tasso di cambio Monero live in molte valute diverse, gratis per tutti."; // Suchmaschinen Beschreibung / Text
+$meta_keywords = "Monero, XMR, fiat, valore, tasso, live, scambio, conversione"; // Suchmaschinen Keywords (angeblich nutzlos)
+
+$title_h1 = "conversione in"; // ↓ XMR umrechnung nach ↓
+
+// Info Text
+$info = "I tassi di cambio su questo sito sono solo a scopo informativo. Non è garantita la loro accuratezza e sono soggetti a modifiche senza preavviso. I tassi di cambio sono aggiornati ogni 5 secondi. Ultimo alle <u>".$time."</u>, Europa/Berlino. Dati forniti da <a class='text-white' href='https://www.coingecko.com/it/monete/monero' hreflang='it' rel='external' target='_blank'>CoinGecko</a>.<br/><a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>Github</a>";
+
+// Tooltip Titel
+$l_eur = "Euro";
+$l_btc = "Bitcoin";
+$l_chf = "Franco Svizzero";
+$l_usd = "Dollaro degli Stati Uniti";
+$l_ltc = "Litecoin";
+$l_gbp = "Sterlina GBP";
+$l_rub = "Rublo Russo";
+$l_jpy = "Yen Giapponese";
+$l_try = "Lira Turca";
+$l_cad = "Dollaro Canadese";
+$l_aud = "Dollaro Australiano";
+$l_hkd = "Dollaro di Hong Kong";
+$l_sgd = "Dollaro di Singapore";
+$l_pln = "Złoty Polacco";
+$l_zar = "ZAR sudafricano";
+$l_inr = "Rupia Indiana";
+$l_aed = "UAE Dirham";
+$l_eth = "Ethereum";
+$l_uah = "Hryvnia"; 
+$l_krw = "Won Sudcoreano";
+$l_brl = "Real Brasiliano"; 
+$l_myr = "Ringgit Brasiliano"; 
+$l_cny = "Renminbi Cinese"; 
+$l_xag = "Argento (31,1g)";
+$l_xau = "Oro (31,1g)";
+$l_vnd = "Dong Vietnamita";
+$l_vef = "Blívar Venezuelano";
+$l_thb = "Baht";
+$l_sar = "Saudi Riyal";
+$l_sek = "Corona Svedese";
+$l_pkr = "Rupia Pakistana";
+$l_nzd = "Dollaro della Nuova Zelanda";
+$l_php = "Peso Filippino";
+$l_nok = "Corona Norvegese";
+$l_lkr = "Rupia di Sri Lanka";
+$l_mmk = "Kyat del Myanmar";
+$l_huf = "Forint Ungarese";
+$l_ils = "New Shekel Istaeliano";
+$l_kwd = "Dinar del Kuwaiti";
+$l_ngn = "Naira Nigeriano";
+$l_idr = "Rupia Indonesiana";
+$l_twd = "Dollaro Neotaiwanese";
+$l_ars = "Peso Argentino";
+$l_bdt = "Taka Bangladese";
+$l_bhd = "Dinar Bahraini"; 
+$l_bmd = "Dollaro delle Bermuda"; 
+$l_czk = "Corona Ceca";
+$l_dkk = "Corona Danese";
+
+// Link di Monero
+$getmonero = '<a class="text-white" href="https://www.getmonero.org/it/" hreflang="it" target="_blank" rel="external">Sito ufficiale</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Community Crowdfunding System (CCS)</a> | <a class="text-white" href="https://moneroecosystem.org/" hreflang="en" target="_blank" rel="external">Ecosistema Monero</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a> | <a class="text-white" href="https://t.me/moneroitalia" hreflang="en" target="_blank" rel="external">Telegram - Monero Italia</a>';
+
+?>
