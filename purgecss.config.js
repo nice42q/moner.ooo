ddcac1b168daf396e9ce7e990a45d2c54dd12489
thecashewtrader <thecashewtrader@protonmail.com>
2024-05-24 00:37:26 +0530

    add purgecss

---
+++ purgecss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	content: ['index.php'],
+	css: ['css/bootstrap.min.css'],
+	output: 'css/main.css',
+	safelist: ['tooltip', 'fade', 'show', 'bs-tooltip-top', 'tooltip-inner', 'tooltip-arrow']
+}
