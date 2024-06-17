7cdd1645c38035f72ee14d5cd928c55b4404a37b
kjekkopp <batiz@tuta.io>
2024-06-17 07:18:33 +0000

    Translated pt-br.php

--- lang/pt-br.php
+++ lang/pt-br.php
@@ -2,77 +2,77 @@
 // For the HTML meta specification, e.g. <!DOCTYPE html><html lang="en">
 $lang_meta = "pt"; // https://www.w3schools.com/tags/ref_language_codes.asp
 
-$page_title = "XMR conversion to EUR/BTC/CHF/USD and many more"; // The browser tab title or search engine title
-$meta_description = "The Monero live exchange rate in many different currencies, free for everyone."; // Search engine description / text
-$meta_keywords = "Monero, XMR, fiat, value, rate, live, exchange, conversion"; // Search engine keywords
+$page_title = "Converter XMR para BRL/EUR/BTC/USD e muito mais"; // The browser tab title or search engine title
+$meta_description = "A cotação em tempo real de Monero em várias moedas, de graça a todos."; // Search engine description / text
+$meta_keywords = "Monero, XMR, fiduciario, valor, taxa, agora, cotacao, conversao"; // Search engine keywords
 
-$title_h1 = "conversion to"; // ↓ XMR [...] ↓
+$title_h1 = "converter para"; // ↓ XMR [...] ↓
 
-$moneroooTable = "Service provided by <a href='https://moner.ooo/'>Moner.ooo</a>, Data provided by <a href='https://www.coingecko.com/pt/moedas/monero' hreflang='pt' rel='external'>CoinGecko</a>";
+$moneroooTable = "Este serviço é oferecido pelo <a href='https://moner.ooo/'>Moner.ooo</a>, os dados são providos pela <a href='https://www.coingecko.com/pt/moedas/monero' hreflang='pt' rel='external'>CoinGecko</a>";
 
 // Info Text
-$info = "The exchange rates on this site are for information purposes only. They are not guaranteed to be accurate, and are subject to change without notice. The exchange rates are updated every 5 Seconds. Last at <u title='Hours:Minutes:Seconds (hh:mm:ss)'>".$time."</u> clock, Europe/Berlin. Data provided by <a class='text-white' href='https://www.coingecko.com/pt/moedas/monero' hreflang='pt' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='en'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo donation page'></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/' class='text-white' rel='external' hreflang='en'>Kuno – Fundraise with Monero</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a>";
+$info = "As cotações oferecidas neste site são apenas para fins informativos. Não há garantia que as cotações estejam precisas e estão sujeitas à mudanças sem aviso prévio. As cotações são atualizadas a cada 5 segundos. A última atualização foi em <u title='Hours:Minutes:Seconds (hh:mm:ss)'>".$time."</u>, horário europeu (Berlin). Os dados são providos pela <a class='text-white' href='https://www.coingecko.com/pt/moedas/monero' hreflang='pt' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='en'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo donation page'></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/' class='text-white' rel='external' hreflang='en'>Kuno – Faça vaquinhas com Monero</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a>";
 
-$clipboard_copy_tooltip = "Copy to clipboard";
+$clipboard_copy_tooltip = "Copiar para a área de transferência";
 
-$l_fiatSelect = "Currency choice";
-$l_fiatInput = "Fiat value input field";
-$l_xmrInput = "Monero value input field";
+$l_fiatSelect = "Moeda selecionada";
+$l_fiatInput = "Valor em moeda fiduciária";
+$l_xmrInput = "Valor em Monero";
 
 // Tooltip Titel
 $l_eur = "Euro";
 $l_btc = "Bitcoin";
-$l_chf = "Swiss Franc";
-$l_usd = "US Dollar";
+$l_chf = "Franco suíço";
+$l_usd = "Dólar americano";
 $l_ltc = "Litecoin";
-$l_gbp = "Pound sterling";
-$l_rub = "Russian Ruble";
-$l_jpy = "Yen";
-$l_try = "Turkish Lira";
-$l_cad = "Canadian Dollar";
-$l_aud = "Australian Dollar";
-$l_hkd = "Hong Kong Dollar";
-$l_sgd = "Singapore Dollar";
-$l_pln = "Złoty";
-$l_zar = "South African Rand";
-$l_inr = "Indian Rupee";
-$l_aed = "UAE Dirham";
+$l_gbp = "Libra esterlina";
+$l_rub = "Rublo russo";
+$l_jpy = "Yen japonês";
+$l_try = "Lira turca";
+$l_cad = "Dólar canadense";
+$l_aud = "Dólar australiano";
+$l_hkd = "Dólar de Hong Kong";
+$l_sgd = "Dólar singapuriano";
+$l_pln = "Złoty polonês";
+$l_zar = "Rand sul-africano";
+$l_inr = "Rúpia indiana";
+$l_aed = "Dirham";
 $l_eth = "Ethereum";
-$l_uah = "Hryvnia"; 
-$l_krw = "South Korean won";
-$l_brl = "Brazilian Real"; 
-$l_myr = "Malaysian Ringgit"; 
-$l_cny = "Renminbi"; 
-$l_xag = "Silver (troy ounce)";
-$l_xau = "Gold (troy ounce)";
-$l_vnd = "Vietnamese dong";
-$l_vef = "Venezuelan bolívar";
-$l_thb = "Baht";
-$l_sar = "Saudi Riyal";
-$l_sek = "Shwedish Krona";
-$l_pkr = "Pakistani Rupee";
-$l_nzd = "New Zealand Dollar";
-$l_php = "Philippine Peso";
-$l_nok = "Norwegian Krone";
-$l_lkr = "Sri Lankan Rupee";
-$l_mmk = "Myanmar Kyat";
-$l_huf = "Hungarian Forint";
-$l_ils = "Israeli New Shekel";
-$l_kwd = "Kuwaiti Dinar";
-$l_ngn = "Nigerian Naira";
-$l_idr = "Indonesian Rupiah";
-$l_twd = "New Taiwan Dollar";
-$l_ars = "Argentine Peso";
-$l_bdt = "Bangladeshi Taka";
-$l_bhd = "Bahraini Dinar"; 
-$l_bmd = "Bermudan Dollar"; 
-$l_czk = "Czech Koruna";
-$l_dkk = "Danish Krone";
-$l_clp = "Chilean Peso";
-$l_mxn = "Mexican Peso";
+$l_uah = "Hryvnia ucraniana"; 
+$l_krw = "Won sul-coreano";
+$l_brl = "Real brasileiro"; 
+$l_myr = "Ringgit malaio"; 
+$l_cny = "Yuan chinês"; 
+$l_xag = "Prata (onça troy)";
+$l_xau = "Ouro (onça troy)";
+$l_vnd = "Dong vietnamita";
+$l_vef = "Bolívar venezuelano";
+$l_thb = "Baht tailandês";
+$l_sar = "Riyal saudita";
+$l_sek = "Krona sueca";
+$l_pkr = "Rúpia paquistanesa";
+$l_nzd = "Dólar neozelandês";
+$l_php = "Peso filipino";
+$l_nok = "Krone norueguês";
+$l_lkr = "Rúpia cingalesa";
+$l_mmk = "Quiate de Mianmar";
+$l_huf = "Forint húngaro";
+$l_ils = "Novo Shekel israelense";
+$l_kwd = "Dinar kuwaitiano";
+$l_ngn = "Naira nigeriana";
+$l_idr = "Rúpia indonésia";
+$l_twd = "Dólar taiwanês";
+$l_ars = "Peso argentino";
+$l_bdt = "Taka bengalês";
+$l_bhd = "Dinar bareinita"; 
+$l_bmd = "Dólar bermudense"; 
+$l_czk = "Koruna tcheca";
+$l_dkk = "Krone dinamarquês";
+$l_clp = "Peso chileno";
+$l_mxn = "Peso mexicano";
 
 // More Monero links
-$getmonero = '<a class="text-white" href="https://www.getmonero.org/pt-br/" hreflang="pt" target="_blank" rel="external">Official website</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Community Crowdfunding System (CCS)</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a>';
+$getmonero = '<a class="text-white" href="https://www.getmonero.org/pt-br/" hreflang="pt" target="_blank" rel="external">Site oficial</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Sistema de vaquinhas comunitárias (CCS)</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a>';
 $countrymonero = ' | <a class="text-white" href="https://t.me/monerobrasil" hreflang="en" target="_blank" rel="external">Telegram - Monero Brasil</a>';
 
 ?>
