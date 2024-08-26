39cf9f728a7829f9f2589c1976cbfd9c00833033
Luke <17616949+nice42q@users.noreply.github.com>
2024-08-26 15:30:42 +0200

    Added Farsi

---
+++ lang/fa.php
@@ -0,0 +1,78 @@
+<?php
+// For the HTML meta specification, e.g. <!DOCTYPE html><html lang="en">
+$lang_meta = "fa"; // https://www.w3schools.com/tags/ref_language_codes.asp
+
+$page_title = "XMR conversion to EUR/BTC/CHF/USD and many more"; // The browser tab title or search engine title
+$meta_description = "The Monero live exchange rate in many different currencies, free for everyone."; // Search engine description / text
+$meta_keywords = "Monero, XMR, fiat, value, rate, live, exchange, conversion"; // Search engine keywords
+
+$title_h1 = "conversion to"; // ↓ XMR [...] ↓
+
+$moneroooTable = "Service provided by <a href='https://moner.ooo/'>Moner.ooo</a>, Data provided by <a href='https://www.coingecko.com/en/coins/monero' hreflang='en' rel='external'>CoinGecko</a>";
+
+// Info Text
+$info = "The exchange rates on this site are for information purposes only. They are not guaranteed to be accurate, and are subject to change without notice. The exchange rates are updated every 5 Seconds. Last at <u title='Hours:Minutes:Seconds (hh:mm:ss)'>".$time."</u> clock, Europe/Berlin. Data provided by <a class='text-white' href='https://www.coingecko.com/en/coins/monero' hreflang='en' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='en'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo donation page'></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/' class='text-white' rel='external' hreflang='en'>Kuno – Fundraise with Monero</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a> | <a style='text-decoration:none; font-weight:bold;' class='text-white' href='https://servers.guru/' hreflang='en' rel='external' target='_blank'>Webhosting provided by<img loading='lazy' src='./img/servers-guru.svg' height='19' alt='Servers Guru' title='Servers Guru' /></a>";
+
+$clipboard_copy_tooltip = "Copy to clipboard";
+
+$l_fiatSelect = "Currency choice";
+$l_fiatInput = "Fiat value input field";
+$l_xmrInput = "Monero value input field";
+
+// Tooltip Titel
+$l_eur = "Euro";
+$l_btc = "Bitcoin";
+$l_chf = "Swiss Franc";
+$l_usd = "US Dollar";
+$l_ltc = "Litecoin";
+$l_gbp = "Pound sterling";
+$l_rub = "Russian Ruble";
+$l_jpy = "Yen";
+$l_try = "Turkish Lira";
+$l_cad = "Canadian Dollar";
+$l_aud = "Australian Dollar";
+$l_hkd = "Hong Kong Dollar";
+$l_sgd = "Singapore Dollar";
+$l_pln = "Złoty";
+$l_zar = "South African Rand";
+$l_inr = "Indian Rupee";
+$l_aed = "UAE Dirham";
+$l_eth = "Ethereum";
+$l_uah = "Hryvnia"; 
+$l_krw = "South Korean won";
+$l_brl = "Brazilian Real"; 
+$l_myr = "Malaysian Ringgit"; 
+$l_cny = "Renminbi"; 
+$l_xag = "Silver (troy ounce)";
+$l_xau = "Gold (troy ounce)";
+$l_vnd = "Vietnamese dong";
+$l_vef = "Venezuelan bolívar";
+$l_thb = "Baht";
+$l_sar = "Saudi Riyal";
+$l_sek = "Swedish Krona";
+$l_pkr = "Pakistani Rupee";
+$l_nzd = "New Zealand Dollar";
+$l_php = "Philippine Peso";
+$l_nok = "Norwegian Krone";
+$l_lkr = "Sri Lankan Rupee";
+$l_mmk = "Myanmar Kyat";
+$l_huf = "Hungarian Forint";
+$l_ils = "Israeli New Shekel";
+$l_kwd = "Kuwaiti Dinar";
+$l_ngn = "Nigerian Naira";
+$l_idr = "Indonesian Rupiah";
+$l_twd = "New Taiwan Dollar";
+$l_ars = "Argentine Peso";
+$l_bdt = "Bangladeshi Taka";
+$l_bhd = "Bahraini Dinar"; 
+$l_bmd = "Bermudan Dollar"; 
+$l_czk = "Czech Koruna";
+$l_dkk = "Danish Krone";
+$l_clp = "Chilean Peso";
+$l_mxn = "Mexican Peso";
+
+// More Monero links
+$getmonero = '<a class="text-white" href="https://www.getmonero.org/" hreflang="en" target="_blank" rel="external">Official website</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Community Crowdfunding System (CCS)</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a>';
+$countrymonero = ' | <a class="text-white" href="https://t.me/monero" hreflang="en" target="_blank" rel="external">Telegram - Monero XMR</a>';
+
+?>
