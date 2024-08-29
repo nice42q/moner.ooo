289da3d30b8e28b942a78dbb7a98d1aa3eeee063
Luke <17616949+nice42q@users.noreply.github.com>
2024-08-29 15:12:58 +0200

    Add files via upload

--- lang/el.php
+++ lang/el.php
@@ -2,77 +2,77 @@
 // For the HTML meta specification, e.g. <!DOCTYPE html><html lang="en">
 $lang_meta = "el"; // https://www.w3schools.com/tags/ref_language_codes.asp
 
-$page_title = "XMR conversion to EUR/BTC/CHF/USD and many more"; // The browser tab title or search engine title
-$meta_description = "The Monero live exchange rate in many different currencies, free for everyone."; // Search engine description / text
-$meta_keywords = "Monero, XMR, fiat, value, rate, live, exchange, conversion"; // Search engine keywords
+$page_title = "XMR μετατροπή σε EUR/BTC/CHF/USD και πολλά άλλα"; // The browser tab title or search engine title
+$meta_description = "Η ζωντανή ισοτιμία του Monero σε πολλά διαφορετικά νομίσματα, δωρεάν για όλους."; // Search engine description / text
+$meta_keywords = "Monero, XMR, fiat, αξία, ισοτιμία, ζωντανά, ανταλλαγή, μετατροπή"; // Search engine keywords
 
-$title_h1 = "conversion to"; // ↓ XMR [...] ↓
+$title_h1 = "μετατροπή σε"; // ↓ XMR [...] ↓
 
-$moneroooTable = "Service provided by <a href='https://moner.ooo/'>Moner.ooo</a>, Data provided by <a href='https://www.coingecko.com/el/coins/monero' hreflang='el' rel='external'>CoinGecko</a>";
+$moneroooTable = "Υπηρεσία παρέχεται από <a href='https://moner.ooo/'>Moner.ooo</a>, Δεδομένα παρέχονται από <a href='https://www.coingecko.com/el/coins/monero' hreflang='el' rel='external'>CoinGecko</a>";
 
 // Info Text
-$info = "The exchange rates on this site are for information purposes only. They are not guaranteed to be accurate, and are subject to change without notice. The exchange rates are updated every 5 Seconds. Last at <u title='Hours:Minutes:Seconds (hh:mm:ss)'>".$time."</u> clock, Europe/Berlin. Data provided by <a class='text-white' href='https://www.coingecko.com/el/coins/monero' hreflang='el' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='en'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo donation page'></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/' class='text-white' rel='external' hreflang='en'>Kuno – Fundraise with Monero</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a> | <a style='text-decoration:none; font-weight:bold;' class='text-white' href='https://servers.guru/' hreflang='en' rel='external' target='_blank'>Webhosting provided by<img loading='lazy' src='./img/servers-guru.svg' height='19' alt='Servers Guru' title='Servers Guru' /></a>";
+$info = "Οι ισοτιμίες σε αυτή την ιστοσελίδα είναι μόνο για ενημερωτικούς σκοπούς. Δεν είναι εγγυημένο ότι είναι ακριβείς και υπόκεινται σε αλλαγές χωρίς προειδοποίηση. Οι ισοτιμίες ενημερώνονται κάθε 5 δευτερόλεπτα. Τελευταία ενημέρωση στις <u title='Ώρες:Λεπτά:Δευτερόλεπτα (ώ:λλ:δδ)'>".$time."</u> ώρα, Ευρώπη/Βερολίνο. Δεδομένα παρέχονται από <a class='text-white' href='https://www.coingecko.com/el/coins/monero' hreflang='el' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='en'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Σελίδα δωρεών Moner.ooo'></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/' class='text-white' rel='external' hreflang='en'>Kuno – Συγκέντρωση κεφαλαίων με Monero</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a> | <a style='text-decoration:none; font-weight:bold;' class='text-white' href='https://servers.guru/' hreflang='en' rel='external' target='_blank'>Φιλοξενία ιστότοπου παρέχεται από<img loading='lazy' src='./img/servers-guru.svg' height='19' alt='Servers Guru' title='Servers Guru' /></a>";
 
-$clipboard_copy_tooltip = "Copy to clipboard";
+$clipboard_copy_tooltip = "Αντιγραφή στο πρόχειρο";
 
-$l_fiatSelect = "Currency choice";
-$l_fiatInput = "Fiat value input field";
-$l_xmrInput = "Monero value input field";
+$l_fiatSelect = "Επιλογή νομίσματος";
+$l_fiatInput = "Πεδίο εισαγωγής αξίας fiat";
+$l_xmrInput = "Πεδίο εισαγωγής αξίας Monero";
 
 // Tooltip Titel
-$l_eur = "Euro";
+$l_eur = "Ευρώ";
 $l_btc = "Bitcoin";
-$l_chf = "Swiss Franc";
-$l_usd = "US Dollar";
+$l_chf = "Ελβετικό Φράγκο";
+$l_usd = "Δολάριο ΗΠΑ";
 $l_ltc = "Litecoin";
-$l_gbp = "Pound sterling";
-$l_rub = "Russian Ruble";
-$l_jpy = "Yen";
-$l_try = "Turkish Lira";
-$l_cad = "Canadian Dollar";
-$l_aud = "Australian Dollar";
-$l_hkd = "Hong Kong Dollar";
-$l_sgd = "Singapore Dollar";
-$l_pln = "Złoty";
-$l_zar = "South African Rand";
-$l_inr = "Indian Rupee";
-$l_aed = "UAE Dirham";
+$l_gbp = "Λίρα στερλίνα";
+$l_rub = "Ρωσικό Ρούβλι";
+$l_jpy = "Γιεν";
+$l_try = "Τουρκική Λίρα";
+$l_cad = "Κανανδικό Δολάριο";
+$l_aud = "Αυστραλιανό Δολάριο";
+$l_hkd = "Δολάριο Χονγκ Κονγκ";
+$l_sgd = "Δολάριο Σιγκαπούρης";
+$l_pln = "Ζλότυ";
+$l_zar = "Νότιο Αφρικανικό Ραντ";
+$l_inr = "Ινδική Ρουπία";
+$l_aed = "Ντιρχάμ ΗΑΕ";
 $l_eth = "Ethereum";
-$l_uah = "Hryvnia"; 
-$l_krw = "South Korean won";
-$l_brl = "Brazilian Real"; 
-$l_myr = "Malaysian Ringgit"; 
-$l_cny = "Renminbi"; 
-$l_xag = "Silver (troy ounce)";
-$l_xau = "Gold (troy ounce)";
-$l_vnd = "Vietnamese dong";
-$l_vef = "Venezuelan bolívar";
-$l_thb = "Baht";
-$l_sar = "Saudi Riyal";
-$l_sek = "Swedish Krona";
-$l_pkr = "Pakistani Rupee";
-$l_nzd = "New Zealand Dollar";
-$l_php = "Philippine Peso";
-$l_nok = "Norwegian Krone";
-$l_lkr = "Sri Lankan Rupee";
-$l_mmk = "Myanmar Kyat";
-$l_huf = "Hungarian Forint";
-$l_ils = "Israeli New Shekel";
-$l_kwd = "Kuwaiti Dinar";
-$l_ngn = "Nigerian Naira";
-$l_idr = "Indonesian Rupiah";
-$l_twd = "New Taiwan Dollar";
-$l_ars = "Argentine Peso";
-$l_bdt = "Bangladeshi Taka";
-$l_bhd = "Bahraini Dinar"; 
-$l_bmd = "Bermudan Dollar"; 
-$l_czk = "Czech Koruna";
-$l_dkk = "Danish Krone";
-$l_clp = "Chilean Peso";
-$l_mxn = "Mexican Peso";
+$l_uah = "Γρίβνα"; 
+$l_krw = "Νοτιοκορεατικό Γουόν";
+$l_brl = "Βραζιλιάνικο Ρεάλ"; 
+$l_myr = "Μαλαισιανό Ρίνγκιτ"; 
+$l_cny = "Ρενμίνμπι"; 
+$l_xag = "Ασήμι (τρόι ουγγιά)";
+$l_xau = "Χρυσός (τρόι ουγγιά)";
+$l_vnd = "Βιετναμέζικο Ντονγκ";
+$l_vef = "Βενεζουελανικό Μπολιβάρ";
+$l_thb = "Μπατ";
+$l_sar = "Σαουδαραβικό Ριάλ";
+$l_sek = "Σουηδική Κορώνα";
+$l_pkr = "Πακιστανική Ρουπία";
+$l_nzd = "Δολάριο Νέας Ζηλανδίας";
+$l_php = "Πέσο Φιλιππίνων";
+$l_nok = "Νορβηγική Κορώνα";
+$l_lkr = "Ρουπία Σρι Λάνκα";
+$l_mmk = "Κυάτ Μιανμάρ";
+$l_huf = "Ουγγρικό Φιορίνι";
+$l_ils = "Νέο Ισραηλινό Σέκελ";
+$l_kwd = "Κουβεϊτιανό Δηνάριο";
+$l_ngn = "Ναϊρα Νιγηρίας";
+$l_idr = "Ρουπία Ινδονησίας";
+$l_twd = "Νέο Δολάριο Ταϊβάν";
+$l_ars = "Πέσο Αργεντινής";
+$l_bdt = "Τάκα Μπαγκλαντές";
+$l_bhd = "Δηνάριο Μπαχρέιν";
+$l_bmd = "Δολάριο Βερμούδων";
+$l_czk = "Τσεχική Κορώνα";
+$l_dkk = "Δανική Κορώνα";
+$l_clp = "Πέσο Χιλής";
+$l_mxn = "Πέσο Μεξικού";
 
 // More Monero links
-$getmonero = '<a class="text-white" href="https://www.getmonero.org/el/" hreflang="el" target="_blank" rel="external">Official website</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Community Crowdfunding System (CCS)</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a>';
+$getmonero = '<a class="text-white" href="https://www.getmonero.org/el/" hreflang="el" target="_blank" rel="external">Επίσημος ιστότοπος</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Σύστημα Κοινοτικής Χρηματοδότησης (CCS)</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a>';
 $countrymonero = ' | <a class="text-white" href="https://t.me/monerogr" hreflang="en" target="_blank" rel="external">Telegram - Monero Greece</a>';
 
 ?>
