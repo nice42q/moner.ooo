acc1673d775316a886636cd016525ae69de4b86a
Monerooo <mail@moner.ooo>
2024-11-24 22:11:07 +0000

    Translated using Weblate (Russian)
    Currently translated at 100.0% (64 of 64 strings)
    
    Translation: Moner.ooo/Moner.ooo
    Translate-URL: https://hosted.weblate.org/projects/monerooo/monerooo/ru/

--- lang/ru.php
+++ lang/ru.php
@@ -1,24 +1,22 @@
 <?php
 // For the HTML meta specification, e.g. <!DOCTYPE html><html lang="en">
-$lang_meta = "ru"; // https://www.w3schools.com/tags/ref_language_codes.asp
-
-$page_title = "Конвертация XMR в EUR/BTC/CHF/USD и многие другие"; // The browser tab title or search engine title
-$meta_description = "Живой курс обмена Monero на многие различные валюты, бесплатно для всех."; // Search engine description / text
-$meta_keywords = "Monero, XMR, фиат, стоимость, курс, живой, обмен, конвертация"; // Search engine keywords
-
-$title_h1 = "конвертация в"; // ↓ XMR [...] ↓
-
+// https://www.w3schools.com/tags/ref_language_codes.asp
+$lang_meta = "ru";
+// The browser tab title or search engine title
+$page_title = "Конвертация XMR в EUR/BTC/CHF/USD и многие другие";
+// Search engine description / text
+$meta_description = "Живой курс обмена Monero на многие различные валюты, бесплатно для всех.";
+// Search engine keywords
+$meta_keywords = "Monero, XMR, фиат, стоимость, курс, живой, обмен, конвертация";
+// ↓ XMR [...] ↓
+$title_h1 = "конвертация в";
 $moneroooTable = "Сервис предоставлен <a href='https://moner.ooo/'>Moner.ooo</a>, Данные предоставлены <a href='https://www.coingecko.com/ru/%D0%9A%D1%80%D0%B8%D0%BF%D1%82%D0%BE%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D1%8B/%D0%9C%D0%BE%D0%BD%D0%B5%D1%80%D0%BE' hreflang='ru' rel='external'>CoinGecko</a>";
-
 // Info Text
-$info = "Курсы обмена, представленные на этом сайте, носят исключительно информационный характер. Их точность не гарантируется, и они могут быть изменены без предварительного уведомления. Курсы обмена обновляются каждые 5 секунд. Последнее обновление <u title='Hours:Minutes:Seconds (hh:mm:ss)'>".$time."</u>, Europe/Berlin. Данные предоставлены <a class='text-white' href='https://www.coingecko.com/ru/%D0%9A%D1%80%D0%B8%D0%BF%D1%82%D0%BE%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D1%8B/%D0%9C%D0%BE%D0%BD%D0%B5%D1%80%D0%BE' hreflang='ru' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='en'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo страница для донатов'></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/' class='text-white' rel='external' hreflang='en'>Kuno – Сбор средств с помощью Monero</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a> | <a style='text-decoration:none; font-weight:bold;' class='text-white' href='https://servers.guru/' hreflang='en' rel='external' target='_blank'>Webhosting provided by<img loading='lazy' src='./img/servers-guru.svg' height='19' alt='Servers Guru' title='Servers Guru' /></a>";
-
+$info = "Курсы обмена, представленные на этом сайте, носят исключительно информационный характер. Их точность не гарантируется, и они могут быть изменены без предварительного уведомления. Курсы обмена обновляются каждые 5 секунд. Последнее обновление <u title='Hours:Minutes:Seconds (hh:mm:ss)'>$time</u>, Europe/Berlin. Данные предоставлены <a class='text-white' href='https://www.coingecko.com/ru/%D0%9A%D1%80%D0%B8%D0%BF%D1%82%D0%BE%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D1%8B/%D0%9C%D0%BE%D0%BD%D0%B5%D1%80%D0%BE' hreflang='ru' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='en'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo страница для донатов'></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/' class='text-white' rel='external' hreflang='en'>Kuno – Сбор средств с помощью Monero</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a> | <a style='text-decoration:none; font-weight:bold;' class='text-white' href='https://servers.guru/' hreflang='en' rel='external' target='_blank'>Webhosting provided by<img loading='lazy' src='./img/servers-guru.svg' height='19' alt='Servers Guru' title='Servers Guru' /></a>";
 $clipboard_copy_tooltip = "Копировать в буфер обмена";
-
 $l_fiatSelect = "Выбор валюты";
 $l_fiatInput = "Поле ввода значений в фиате";
 $l_xmrInput = "Поле ввода значения в Monero";
-
 // Tooltip Titel
 $l_eur = "Евро";
 $l_btc = "Bitcoin";
@@ -71,9 +69,6 @@ $l_dkk = "Датская крона";
 $l_clp = "Чилийское песо";
 $l_mxn = "Мексиканское песо";
 $l_gel = "Georgian Lari";
-
 // More Monero links
 $getmonero = '<a class="text-white" href="https://www.getmonero.org/ru/" hreflang="ru" target="_blank" rel="external">Официальный сайт</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Система краудфандинга для сообществ (CCS)</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Обозреватель Monero</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a>';
 $countrymonero = ' | <a class="text-white" href="https://t.me/xmr_ru" hreflang="en" target="_blank" rel="external">Telegram - Monero XMR.RU</a>';
-
-?>
