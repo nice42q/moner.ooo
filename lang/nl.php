68403b4daa8d2f28849c150a8527ed1b5822c42e
Monerooo <mail@moner.ooo>
2024-11-24 22:11:07 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (64 of 64 strings)
    
    Translation: Moner.ooo/Moner.ooo
    Translate-URL: https://hosted.weblate.org/projects/monerooo/monerooo/nl/

--- lang/nl.php
+++ lang/nl.php
@@ -1,24 +1,22 @@
 <?php
 // For the HTML meta specification, e.g. <!DOCTYPE html><html lang="en">
-$lang_meta = "nl"; // https://www.w3schools.com/tags/ref_language_codes.asp
-
-$page_title = "XMR conversie naar EUR/BTC/CHF/USD en vele anderen"; // The browser tab title or search engine title
-$meta_description = "De live wisselkoers van Monero in veel verschillende valuta's, gratis voor iedereen."; // Search engine description / text
-$meta_keywords = "Monero, XMR, fiat, waarde, koers, live, wissel, conversie"; // Search engine keywords
-
-$title_h1 = "conversie naar"; // ↓ XMR [...] ↓
-
+// https://www.w3schools.com/tags/ref_language_codes.asp
+$lang_meta = "nl";
+// The browser tab title or search engine title
+$page_title = "XMR conversie naar EUR/BTC/CHF/USD en vele anderen";
+// Search engine description / text
+$meta_description = "De live wisselkoers van Monero in veel verschillende valuta's, gratis voor iedereen.";
+// Search engine keywords
+$meta_keywords = "Monero, XMR, fiat, waarde, koers, live, wissel, conversie";
+// ↓ XMR [...] ↓
+$title_h1 = "conversie naar";
 $moneroooTable = "Dienst geleverd door <a href='https://moner.ooo/'>Moner.ooo</a>, Gegevens geleverd door <a href='https://www.coingecko.com/nl/coins/monero' hreflang='nl' rel='external'>CoinGecko</a>";
-
 // Info Text
-$info = "De wisselkoersen op deze site zijn alleen voor informatieve doeleinden. Ze zijn niet gegarandeerd nauwkeurig en kunnen zonder kennisgeving worden gewijzigd. De wisselkoersen worden elke 5 seconden bijgewerkt. Laatste update om <u title='Uren:Minuten:Seconden (uu:mm:ss)'>".$time."</u> uur, Europa/Berlijn. Gegevens geleverd door <a class='text-white' href='https://www.coingecko.com/nl/coins/monero' hreflang='nl' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='en'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo donatiepagina'></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/' class='text-white' rel='external' hreflang='en'>Kuno – Fondsen werven met Monero</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a> | <a style='text-decoration:none; font-weight:bold;' class='text-white' href='https://servers.guru/' hreflang='en' rel='external' target='_blank'>Webhosting geleverd door<img loading='lazy' src='./img/servers-guru.svg' height='19' alt='Servers Guru' title='Servers Guru' /></a>";
-
+$info = "De wisselkoersen op deze site zijn alleen voor informatieve doeleinden. Ze zijn niet gegarandeerd nauwkeurig en kunnen zonder kennisgeving worden gewijzigd. De wisselkoersen worden elke 5 seconden bijgewerkt. Laatste update om <u title='Uren:Minuten:Seconden (uu:mm:ss)'>$time</u> uur, Europa/Berlijn. Gegevens geleverd door <a class='text-white' href='https://www.coingecko.com/nl/coins/monero' hreflang='nl' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='en'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo donatiepagina'></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/' class='text-white' rel='external' hreflang='en'>Kuno – Fondsen werven met Monero</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a> | <a style='text-decoration:none; font-weight:bold;' class='text-white' href='https://servers.guru/' hreflang='en' rel='external' target='_blank'>Webhosting geleverd door<img loading='lazy' src='./img/servers-guru.svg' height='19' alt='Servers Guru' title='Servers Guru' /></a>";
 $clipboard_copy_tooltip = "Kopiëren naar klembord";
-
 $l_fiatSelect = "Valutakeuze";
 $l_fiatInput = "Fiat waarde invoerveld";
 $l_xmrInput = "Monero waarde invoerveld";
-
 // Tooltip Titel
 $l_eur = "Euro";
 $l_btc = "Bitcoin";
@@ -38,11 +36,11 @@ $l_zar = "Zuid-Afrikaanse Rand";
 $l_inr = "Indiase Roepie";
 $l_aed = "VAE Dirham";
 $l_eth = "Ethereum";
-$l_uah = "Hryvnia"; 
+$l_uah = "Hryvnia";
 $l_krw = "Zuid-Koreaanse Won";
-$l_brl = "Braziliaanse Real"; 
-$l_myr = "Maleisische Ringgit"; 
-$l_cny = "Renminbi"; 
+$l_brl = "Braziliaanse Real";
+$l_myr = "Maleisische Ringgit";
+$l_cny = "Renminbi";
 $l_xag = "Zilver (troy ounce)";
 $l_xau = "Goud (troy ounce)";
 $l_vnd = "Vietnamese Dong";
@@ -64,16 +62,13 @@ $l_idr = "Indonesische Roepia";
 $l_twd = "Nieuwe Taiwanese Dollar";
 $l_ars = "Argentijnse Peso";
 $l_bdt = "Bengaalse Taka";
-$l_bhd = "Bahreinse Dinar"; 
-$l_bmd = "Bermuda Dollar"; 
+$l_bhd = "Bahreinse Dinar";
+$l_bmd = "Bermuda Dollar";
 $l_czk = "Tsjechische Kroon";
 $l_dkk = "Deense Kroon";
 $l_clp = "Chileense Peso";
 $l_mxn = "Mexicaanse Peso";
 $l_gel = "Georgian Lari";
-
 // More Monero links
 $getmonero = '<a class="text-white" href="https://www.getmonero.org/nl/" hreflang="nl" target="_blank" rel="external">Officiële website</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Community Crowdfunding Systeem (CCS)</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a>';
 $countrymonero = ' | <a class="text-white" href="https://t.me/moneronl" hreflang="en" target="_blank" rel="external">Telegram - MoneroNL</a>';
-
-?>
