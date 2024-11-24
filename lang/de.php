cca18e2d5102f674f4627e59b1d8b460065ac195
Monerooo <mail@moner.ooo>
2024-11-24 22:11:07 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (64 of 64 strings)
    
    Translation: Moner.ooo/Moner.ooo
    Translate-URL: https://hosted.weblate.org/projects/monerooo/monerooo/de/

--- lang/de.php
+++ lang/de.php
@@ -1,24 +1,22 @@
 <?php
 // Für die HTML meta angabe, bspw. <!DOCTYPE html><html lang="de">
-$lang_meta = "de"; // https://www.w3schools.com/tags/ref_language_codes.asp
-
-$page_title = "XMR umrechnung nach EUR/BTC/CHF/USD und viele mehr"; // Der Browser Tab Titel bzw. Suchmaschinen Titel
-$meta_description = "Der Monero live Wechselkurs in vielen verschiedenen Währungen, kostenlos für jeden."; // Suchmaschinen Beschreibung / Text
-$meta_keywords = "Monero, XMR, Fiat, Wert, Kurs, Live, Wechsel, Umrechnung"; // Suchmaschinen Keywords
-
-$title_h1 = "umrechnung nach"; // ↓ XMR [...] ↓
-
+// https://www.w3schools.com/tags/ref_language_codes.asp
+$lang_meta = "de";
+// Der Browser Tab Titel bzw. Suchmaschinen Titel
+$page_title = "XMR umrechnung nach EUR/BTC/CHF/USD und viele mehr";
+// Suchmaschinen Beschreibung / Text
+$meta_description = "Der Monero live Wechselkurs in vielen verschiedenen Währungen, kostenlos für jeden.";
+// Suchmaschinen Keywords
+$meta_keywords = "Monero, XMR, Fiat, Wert, Kurs, Live, Wechsel, Umrechnung";
+// ↓ XMR [...] ↓
+$title_h1 = "umrechnung nach";
 $moneroooTable = "Service bereitgestellt von <a href='https://moner.ooo/'>Moner.ooo</a>, Daten bereitgestellt von <a href='https://www.coingecko.com/de/coins/monero' hreflang='de' rel='external'>CoinGecko</a>";
-
 // Info Text
-$info = "Die Werte auf dieser Webseite dienen nur der Information. Der Wert ist nicht garantiert und wird ohne vorherige Ankündigung geändert. Die Werte werden alle 5 Sekunden aktualisiert. Zuletzt um <u title='Stunden:Minuten:Sekunden (hh:mm:ss)'>".$time."</u> Uhr, Europe/Berlin. Daten bereitgestellt von <a class='text-white' href='https://www.coingecko.com/de/munze/monero' hreflang='de' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='de'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo Spendenseite' /></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/lang/de/' class='text-white' rel='external' hreflang='de'>Kuno – Mit Monero Spenden sammeln</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a> | <a style='text-decoration:none; font-weight:bold;' class='text-white' href='https://servers.guru/' hreflang='en' rel='external' target='_blank'>Webhosting bereitgestellt von<img loading='lazy' src='./img/servers-guru.svg' height='19' alt='Servers Guru' title='Servers Guru' /></a>";
-
+$info = "Die Werte auf dieser Webseite dienen nur der Information. Der Wert ist nicht garantiert und wird ohne vorherige Ankündigung geändert. Die Werte werden alle 5 Sekunden aktualisiert. Zuletzt um <u title='Stunden:Minuten:Sekunden (hh:mm:ss)'>$time</u> Uhr, Europe/Berlin. Daten bereitgestellt von <a class='text-white' href='https://www.coingecko.com/de/munze/monero' hreflang='de' rel='external' target='_blank'>CoinGecko</a>.<br/><a target='_blank' href='https://kuno.anne.media/donate/onml/' rel='external' hreflang='de'><img loading='lazy' src='./img/kuno-monero-26x26.png' width='17' height='17' alt='Kuno - Moner.ooo Spendenseite' /></a>&nbsp;<a target='_blank' href='https://kuno.anne.media/lang/de/' class='text-white' rel='external' hreflang='de'>Kuno – Mit Monero Spenden sammeln</a> | <a class='text-white' href='https://github.com/nice42q/moner.ooo' hreflang='en' rel='external' target='_blank'>GitHub</a> | <a style='text-decoration:none; font-weight:bold;' class='text-white' href='https://servers.guru/' hreflang='en' rel='external' target='_blank'>Webhosting bereitgestellt von<img loading='lazy' src='./img/servers-guru.svg' height='19' alt='Servers Guru' title='Servers Guru' /></a>";
 $clipboard_copy_tooltip = "In die Zwischenablage kopieren";
-
 $l_fiatSelect = "Währungsauswahl";
 $l_fiatInput = "Fiat-Wert Eingabefeld";
 $l_xmrInput = "Monero-Wert Eingabefeld";
-
 // Tooltip Titel
 $l_eur = "Euro";
 $l_btc = "Bitcoin";
@@ -40,9 +38,9 @@ $l_aed = "VAE-Dirham";
 $l_eth = "Ethereum";
 $l_uah = "Hrywnja";
 $l_krw = "Südkoreanischer Won";
-$l_brl = "Brasilianischer Real"; 
-$l_myr = "Malaysischer Ringgit"; 
-$l_cny = "Renminbi"; 
+$l_brl = "Brasilianischer Real";
+$l_myr = "Malaysischer Ringgit";
+$l_cny = "Renminbi";
 $l_xag = "Silber (Feinunze)";
 $l_xau = "Gold (Feinunze)";
 $l_vnd = "Vietnamesischer Đồng";
@@ -64,16 +62,13 @@ $l_idr = "Indonesische Rupiah";
 $l_twd = "Neuer Taiwan-Dollar";
 $l_ars = "Argentinischer Peso";
 $l_bdt = "Taka";
-$l_bhd = "Bahrain-Dinar"; 
-$l_bmd = "Bermuda-Dollar"; 
+$l_bhd = "Bahrain-Dinar";
+$l_bmd = "Bermuda-Dollar";
 $l_czk = "Tschechische Krone";
 $l_dkk = "Dänische Krone";
 $l_clp = "Chilenischer Peso";
 $l_mxn = "Mexikanischer Peso";
 $l_gel = "Georgischer Lari";
-
 // Weitere Monero Links
 $getmonero = '<a class="text-white" href="https://www.getmonero.org/de/" hreflang="de" target="_blank" rel="external">Offizielle Webseite</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Community Crowdfunding System (CCS)</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a>';
 $countrymonero = ' | <a class="text-white" href="https://t.me/moneroger" hreflang="en" target="_blank" rel="external">Telegram - Monero Germany</a>';
-
-?>
