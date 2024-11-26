5acfc99066aa7058bfe2cf5f9c673035da0ffc49
Monerooo <mail@moner.ooo>
2024-11-26 14:04:07 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (66 of 66 strings)
    
    Translation: Moner.ooo/Moner.ooo
    Translate-URL: https://hosted.weblate.org/projects/monerooo/monerooo/de/

--- lang/de.php
+++ lang/de.php
@@ -73,3 +73,4 @@ $l_gel = "Georgischer Lari";
 $getmonero = '<a class="text-white" href="https://www.getmonero.org/de/" hreflang="de" target="_blank" rel="external">Offizielle Webseite</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">Community Crowdfunding System (CCS)</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Monero Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Monero Talk</a>';
 $countrymonero = '&nbsp;| <a class="text-white" href="https://t.me/moneroger" hreflang="en" target="_blank" rel="external">Telegram - Monero Germany</a>';
 $rtl = 'false';
+$unixTime = "Unix Zeit:";
