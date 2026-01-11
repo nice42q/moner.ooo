8cfa7d2e278c1c56b5991b209c70c2f7fec7fd72
Luke <17616949+nice42q@users.noreply.github.com>
2026-01-11 16:25:11 +0100

    Delete purgecss.config.js

--- purgecss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	content: ['index.php'],
-	css: ['css/bootstrap.min.css'],
-	output: 'css/main.css',
-	safelist: ['tooltip', 'fade', 'show', 'bs-tooltip-top', 'tooltip-inner', 'tooltip-arrow']
-}
