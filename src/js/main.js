6b1b59ad2658fe3c8dfc4a0c8f047f4f76d29437
Luke <17616949+nice42q@users.noreply.github.com>
2026-01-11 16:24:29 +0100

    Delete src/js/main.js

--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-import Tooltip from "bootstrap/js/dist/tooltip";
-var tooltipTriggerList = [].slice.call(
-  document.querySelectorAll('[data-toggle="tooltip"]')
-);
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new Tooltip(tooltipTriggerEl, { placement: "top" });
-});
-console.log(tooltipList);
