1f0b9a4e29134135e4c331903ea19fd08fce1927
Oğuz Ersen <oguz@ersen.moe>
2026-07-23 20:38:43 +0200

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (84 of 84 strings)
    
    Translation: Moner.ooo/Moner.ooo
    Translate-URL: https://hosted.weblate.org/projects/monerooo/monerooo/tr/

--- lang/tr.php
+++ lang/tr.php
@@ -1,26 +1,23 @@
 <?php
-$l_lang_meta         = "tr";
-$l_rtl               = "false";
-$l_page_title        = "XMR'nin EUR/BTC/CHF/USD ve birçok diğerine dönüştürülmesi";
-$l_meta_description  = "Monero'nun birçok farklı para biriminde canlı döviz kuru, herkes için ücretsiz.";
-$l_meta_keywords     = "Monero, XMR, fiat, değer, oran, canlı, döviz, dönüşüm";
-
-$l_title_h1          = "dönüştürme";
+$l_lang_meta = "tr";
+$l_rtl = "false";
+$l_page_title = "XMR'nin EUR/BTC/CHF/USD ve birçok diğerine dönüştürülmesi";
+$l_meta_description = "Monero'nun birçok farklı para biriminde canlı döviz kuru, herkes için ücretsiz.";
+$l_meta_keywords = "Monero, XMR, fiat, değer, oran, canlı, döviz, dönüşüm";
+$l_title_h1 = "dönüştürme";
 $l_moneroooTable = "Hizmet sağlayan <a href='https://moner.ooo/'>Moner.ooo</a>, veriler <a href='https://www.coingecko.com/tr/coins/monero' hreflang='tr' rel='external'>CoinGecko</a> tarafından sağlanmıştır";
-$l_fiatSelect        = "Para birimi seçimi";
-$l_fiatInput         = "Fiat değer giriş alanı";
-$l_xmrInput          = "Monero değer giriş alanı";
+$l_fiatSelect = "Para birimi seçimi";
+$l_fiatInput = "Fiat değer giriş alanı";
+$l_xmrInput = "Monero değer giriş alanı";
 $l_clipboard_copy_tooltip = "Panoya kopyala";
 $l_copytoclipboard_success_text = "Kopyalandı!";
-$l_copytoclipboard_error_text   = "Hata!";
-
-$l_refresh_info_minute  = "Döviz kurları dakikada bir güncellenir.";
+$l_copytoclipboard_error_text = "Hata!";
+$l_refresh_info_minute = "Döviz kurları dakikada bir güncellenir.";
 $l_refresh_info_minutes = "Döviz kurları her %s dakikada bir güncellenir.";
 $l_refresh_info_seconds = "Döviz kurları her %s saniyede bir güncellenir.";
-$l_last_at              = "Son güncelleme:";
-$l_hr_min_sec           = "Saat:Dakika:Saniye (ss:dd:sn)";
-$l_unixTime             = "Unix Zamanı:";
-
+$l_last_at = "Son güncelleme:";
+$l_hr_min_sec = "Saat:Dakika:Saniye (ss:dd:sn)";
+$l_unixTime = "Unix Zamanı:";
 $l_aed = "BAE Dirhemi";
 $l_ars = "Arjantin Pezosu";
 $l_aud = "Avustralya Doları";
@@ -72,23 +69,17 @@ $l_vnd = "Vietnam Dongu";
 $l_xag = "Gümüş (troy ons)";
 $l_xau = "Altın (troy ons)";
 $l_zar = "Güney Afrika Randı";
-
-$l_info_main         = "Bu sitedeki döviz kurları yalnızca bilgi amaçlıdır.&nbsp;Doğruluğu garanti edilmez ve haber verilmeksizin değiştirilebilir.";
-
-$l_data_provided     = "Veri sağlayan:";
-$l_cg_url            = "https://www.coingecko.com/tr/coins/monero";
-$l_cg_url_hreflang    = "tr";
-$l_coingecko         = $l_data_provided . " <a class=\"text-white\" href=\"$l_cg_url\" hreflang=\"$l_cg_url_hreflang\" rel=\"external\" target=\"_blank\">CoinGecko</a>.";
-
-$l_getmonero         = '<a class="text-white" href="https://www.getmonero.org/tr/" hreflang="tr" target="_blank" rel="external">Resmi Web Sitesi</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">CCS</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Talk</a>';
-$l_countrymonero     = '';
-
-$l_donation_page     = "bağış sayfası";
+$l_info_main = "Bu sitedeki döviz kurları yalnızca bilgi amaçlıdır.&nbsp;Doğruluğu garanti edilmez ve haber verilmeksizin değiştirilebilir.";
+$l_data_provided = "Veri sağlayan:";
+$l_cg_url = "https://www.coingecko.com/tr/coins/monero";
+$l_cg_url_hreflang = "tr";
+$l_coingecko = "$l_data_provided <a class=\"text-white\" href=\"$l_cg_url\" hreflang=\"$l_cg_url_hreflang\" rel=\"external\" target=\"_blank\">CoinGecko</a>.";
+$l_getmonero = '<a class="text-white" href="https://www.getmonero.org/tr/" hreflang="tr" target="_blank" rel="external">Resmi Web Sitesi</a> | <a class="text-white" href="https://ccs.getmonero.org/" hreflang="en" target="_blank" rel="external">CCS</a> | <a class="text-white" href="https://www.monero.observer/resources/" hreflang="en" target="_blank" rel="external">Observer</a> | <a class="text-white" href="https://www.monerotalk.live/" hreflang="en" target="_blank" rel="external">Talk</a>';
+$l_countrymonero = '&nbsp;| <a class="text-white" href="https://t.me/monero" hreflang="en" target="_blank" rel="external">Telegram XMR</a> | <a class="text-white" href="https://www.revuo-xmr.com/" hreflang="en" target="_blank" rel="external">Revuo Monero</a>';
+$l_donation_page = "bağış sayfası";
 $l_kuno_homepage_url = "https://kuno.anne.media/lang/tr/";
 $l_kuno_homepage_url_hreflang = "tr";
-$l_kuno_text         = "Kuno – Monero ile Bağış Toplama";
+$l_kuno_text = "Kuno – Monero ile Bağış Toplama";
 $l_webserver_sponsor = "Web hosting sağlayan:";
-
-$l_api_update        = $l_last_at . " <u title=\"$l_hr_min_sec\"><span id=\"lastUpdateDisplay\">00:00:00</span></u> (<span id=\"displayTimeZone\">yerel saat</span>).";
-
-$l_info              = $l_info_main."&nbsp;<span id=\"refreshInfo\" class=\"mb-1\">--</span>&nbsp;$l_api_update&nbsp;$l_coingecko<br><a target=\"_blank\" href=\"https://kuno.anne.media/donate/onml/\" rel=\"external\" hreflang=\"en\"><img loading=\"lazy\" align=\"middle\" src=\"./img/kuno-monero-26x26.png\" width=\"17\" height=\"17\" alt=\"Kuno - Moner.ooo $l_donation_page\"></a>&nbsp;<a target=\"_blank\" href=\"$l_kuno_homepage_url\" class=\"text-white\" rel=\"external\" hreflang=\"$l_kuno_homepage_url_hreflang\">$l_kuno_text</a> | <a class=\"text-white\" href=\"https://github.com/nice42q/moner.ooo\" hreflang=\"en\" rel=\"external\" target=\"_blank\">GitHub</a> | <a style=\"text-decoration:none; font-weight:bold;\" class=\"text-white\" href=\"https://servers.guru/\" hreflang=\"en\" rel=\"external\" target=\"_blank\">$l_webserver_sponsor<img loading=\"lazy\" align=\"middle\" src=\"./img/servers-guru.svg\" height=\"19\" alt=\"Servers Guru\" title=\"Servers Guru\"></a>";
+$l_api_update = "$l_last_at <u title=\"$l_hr_min_sec\"><span id=\"lastUpdateDisplay\">00:00:00</span></u> (<span id=\"displayTimeZone\">yerel saat</span>).";
+$l_info = "$l_info_main&nbsp;<span id=\"refreshInfo\" class=\"mb-1\">--</span>&nbsp;$l_api_update&nbsp;$l_coingecko<br><a target=\"_blank\" href=\"https://kuno.anne.media/donate/onml/\" rel=\"external\" hreflang=\"en\"><img loading=\"lazy\" align=\"middle\" src=\"./img/kuno-monero-26x26.png\" width=\"17\" height=\"17\" alt=\"Kuno - Moner.ooo $l_donation_page\"></a>&nbsp;<a target=\"_blank\" href=\"$l_kuno_homepage_url\" class=\"text-white\" rel=\"external\" hreflang=\"$l_kuno_homepage_url_hreflang\">$l_kuno_text</a> | <a class=\"text-white\" href=\"https://github.com/nice42q/moner.ooo\" hreflang=\"en\" rel=\"external\" target=\"_blank\">GitHub</a> | <a style=\"text-decoration:none; font-weight:bold;\" class=\"text-white\" href=\"https://servers.guru/\" hreflang=\"en\" rel=\"external\" target=\"_blank\">$l_webserver_sponsor<img loading=\"lazy\" align=\"middle\" src=\"./img/servers-guru.svg\" height=\"19\" alt=\"Servers Guru\" title=\"Servers Guru\"></a>";
